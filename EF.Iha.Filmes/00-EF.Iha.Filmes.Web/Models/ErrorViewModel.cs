1d66bc50e12e8501d46b1afe3eff9023a646e266
masanori-iha <masanori.iha@outlook.com>
2023-05-20 17:01:00 -0300

    [FEAT] - criando projeto web

---
+++ EF.Iha.Filmes/00-EF.Iha.Filmes.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _00_EF.Iha.Filmes.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
